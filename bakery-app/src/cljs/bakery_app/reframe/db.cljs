401db0ed9b093366aec59f95c64e8a8fd006c14d
Jim Lynch <mrdotjim@gmail.com>
2018-02-12 09:28:44 -0500

    Update db.cljs

--- bakery-app/src/cljs/bakery_app/reframe/db.cljs
+++ bakery-app/src/cljs/bakery_app/reframe/db.cljs
@@ -3,18 +3,4 @@
 (def default-db
   {:name "re-frame"
    :items {}
-   :shoppingCart
-    {
-      :cartItems { :muffin {
-                             :name "derp"
-                              :quantity 1
-                              :subtotal 500 }
-                   :brownie {
-                             :name "brownie"
-                             :quantity 1
-                             :subtotal 500 }
-
-                   }
-      :orderTotalPrice 0
-      }
    })
