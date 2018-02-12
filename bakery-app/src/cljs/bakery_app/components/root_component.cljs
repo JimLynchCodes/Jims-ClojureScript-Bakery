f652510e62f977ab14de324e740d3dd77b0583af
Jim Lynch <mrdotjim@gmail.com>
2018-02-12 10:51:12 -0500

    Update root_component.cljs

--- bakery-app/src/cljs/bakery_app/components/root_component.cljs
+++ bakery-app/src/cljs/bakery_app/components/root_component.cljs
@@ -6,22 +6,18 @@
             [bakery-app.reframe.handlers :as handlers]
             [bakery-app.components.container.product-list :as product-list]
             [bakery-app.components.container.shopping-cart :as shopping-cart]
-;            [bakery-app.components.container.product-list] as product-list]
             ))
 
 (defn root-component []
   (let [name (re-frame/subscribe [::subs/name])]
 
-    (reagent/create-class                 ;; <-- expects a map of functions
-      {:component-did-mount               ;; the name of a lifecycle function
-                      #((re-frame/dispatch [::handlers/load-product-list]))
-
-       ;; other lifecycle funcs can go in here
-
+    (reagent/create-class                
+      {:component-did-mount               
+         #((re-frame/dispatch [::handlers/load-product-list]))
        :display-name  "mail-panel"  ;; for more helpful warnings & errors
 
-       :render        ;; Note:  is not :render
-                      (fn []           ;; remember to repeat parameters
+       :render       
+                      (fn []         
                         [:div {:class "root-component"}
 
                          [product-list/product-list-component]
